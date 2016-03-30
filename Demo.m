10048e854d59f9874b416fa543ed393284dfa147
Xiyu Yi <chinahelenyxy@gmail.com>
2016-03-29 20:57:51 -0700

    added SOFI process and parameter estimation demonstration to the demo code.

--- Demo.m
+++ Demo.m
@@ -3,16 +3,23 @@ addpath ./fcns
 xdim=256;
 ydim=256;
 mvlength=1000;
-inputPath = './ExampleMovie';
+inputPath = '/Volumes/Seagate2/Research/SOFI-ldrc/ForPublish_code_moved_SeeReadMe/SOFI_Modules/ExampleMovie';
 inputName = 'dish1_9_good.tif';
-outputPath = './ExampleMovie';
-ImMeanLocFori = './ExampleMovie/ImMean_dish1_9_good.mat';
+outputPath = '/Volumes/Seagate2/Research/SOFI-ldrc/ForPublish_code_moved_SeeReadMe/SOFI_Modules/ExampleMovie';
+ImMeanLocFori = '/Volumes/Seagate2/Research/SOFI-ldrc/ForPublish_code_moved_SeeReadMe/SOFI_Modules/ExampleMovie/ImMean_dish1_9_good.mat';
 saveOption='off';
 laglist = [0, 0, 0, 0, 0, 0, 0];
 LibPath = './fcns'
 xini=89; yini=160;
 xend=166;yend=250;
 
-%sigFit = xy_getPSF_globalAC2XC2(xdim,ydim,mvlength,inputPath,inputName,outputPath,ImMeanLocFori,saveOption);
+%% step1 estimate the PSF.
 sigFit = xy_getPSF_globalAC2XC2_selectFOV(xdim,ydim,mvlength,inputPath,inputName,outputPath,ImMeanLocFori,saveOption,xini,xend,yini,yend);
-xy_getSOFI_Ord2to7(xdim,ydim,mvlength,inputPath,inputName,outputPath,laglist,LibPath,sigFit);
+
+
+%% step2 SOFI calculation
+%xy_getSOFI_Ord2to7(xdim,ydim,mvlength,inputPath,inputName,outputPath,laglist,LibPath,sigFit);
+
+
+%% step3 parameter estimation
+[RhoMap, RhoTru, epsMap] = xy_getPSF_paraEsti(xdim,ydim,mvlength,inputPath,inputName,outputPath,ImMeanLocFori,sigFit,saveOption,laglist);
